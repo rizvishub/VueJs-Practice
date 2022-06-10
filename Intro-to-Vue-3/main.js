de653367c5307ace4eefbb44ff471c92613a96ed
Zafar Rizvi <zafar.rizvi@zixel.cn>
2022-06-10 10:38:41 +0500

    Uploading  on Github

---
+++ Intro-to-Vue-3/main.js
@@ -0,0 +1,45 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			cart: 0,
+			product: "Socks",
+			brand: "Mastery",
+			selectedVarient: 0,
+			details: ["50% cotton", "30% wool", "20% polyester"],
+			variants: [
+				{
+					id: 2234,
+					color: "green",
+					image: "./assets/images/Green.jpeg",
+					quantity: 6,
+				},
+				{
+					id: 2235,
+					color: "blue",
+					image: "./assets/images/Blue.jpeg",
+					quantity: 6,
+				},
+			],
+		};
+	},
+	methods: {
+		addToCart() {
+			this.cart += 1;
+			this.variants[this.selectedVarient].quantity -= 1;
+		},
+		updateVarient(index) {
+			this.selectedVarient = index;
+		},
+	},
+	computed: {
+		title() {
+			return this.brand + " " + this.product;
+		},
+		image() {
+			return this.variants[this.selectedVarient].image;
+		},
+		inStock() {
+			return this.variants[this.selectedVarient].quantity;
+		},
+	},
+});
